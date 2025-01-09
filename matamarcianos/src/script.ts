5145c8c415f3354d319c2f8c0943fbfcfaecda01
Pau Muñoz Serra <munozserrap@gmail.com>
2025-01-09 17:01:58 +0100

    ..

--- matamarcianos/src/script.ts
+++ matamarcianos/src/script.ts
@@ -1,9 +1,17 @@
+let $ : any;
+
 function moureNauEsquerra() {
     // Lògica per moure la nau a l'esquerra
+    let nau = $('#nau');
+    let pos = nau.position();
+    nau.css('left', pos.left - 10 + 'px');
 }
 
 function moureNauDreta() {
     // Lògica per moure la nau a la dreta
+    let nau = $('#nau');
+    let pos = nau.position();
+    nau.css('left', pos.left + 10 + 'px');
 }
 
 function moureNauAmunt() {
@@ -19,7 +27,22 @@ function atacar() {
 }
 
 function init() {
-    // Moure la nau amb tecles
+    $(document).keydown(function (event: any) {
+        const eventCodiTecla = event.which; // Retorna l'id de la tecla que s'ha premut
+        console.log(`Tecla premuda: ${eventCodiTecla} -- '${event.key}'`);
+        switch (eventCodiTecla) {
+            case 37:
+                moureNauEsquerra();
+                break;
+            case 39:
+                moureNauDreta();
+                break;
+            case 32:
+                atacar();
+                break;
+        }
+
+    });
 
     // Atacar amb tecles
 }
